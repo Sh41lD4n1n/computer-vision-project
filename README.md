79657972bafe45f3b2dd1138af1de932c00bd02f
Denisalik <schegletovdenis@mail.ru>
2022-11-27 22:30:45 +0300

    Add comments

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ manually. Alternatively, they may apply an algorithm which will analyze images a
 number of animals in given images. Thus, the goal
 of this project is to evaluate the number of animals
 in a sequence of images taken from Camera Traps.
+## Structure
+1. In `/app` everything related to service that predict number of animals.
+2. In `/analysis` you can find plots and code for running the model to get the plots
+3. In `/data` you can store everything related to images and related metadata. For example `processed_test`, `processed_train`, files from kaggle and data which were produced by model.
+4. In `model.ipynb` you can find all code related to running the code, preparing the data e.t.c.
+5. In `data.yaml` file we store data to use it for training by yolo torch cli
 ### Application
 To start a server run these commands:
 ```bash
@@ -19,5 +25,7 @@ $ docker-compose build
 $ docker-compose up
 ```
 ### Test using Postman
+##### Use the same settings as here.
+
 ![img.png](postman.png)
 ### Dataset: [Kaggle competition](https://www.kaggle.com/competitions/iwildcam2022-fgvc9/data)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import torch
 from fastapi import FastAPI, UploadFile, File, logger as fa_logger
 from PIL import Image
-import uvicorn
 from typing import List
 from pandas import DataFrame
 
@@ -14,8 +13,18 @@ logger = fa_logger.logger
 
 
 def predict_bboxes(img: List[Image.Image]) -> List[DataFrame]:
+    """
+    predicting bboxes for all images using yolo model
+    :param img: list[Image.Image]
+    :return: list[pd.DatafFrame]
+    """
     def make_bbox(df):
-        filtered_df = df[df['confidence'] > 0.3]
+        """
+        filter bbox by confidence
+        :param df: pd.DataFrame
+        :return: dataframe with bbox
+        """
+        filtered_df = df[df['confidence'] > 0.5]
         bbox = filtered_df[['xmin', 'ymin', 'xmax', 'ymax']]
         return bbox
 
@@ -25,29 +34,52 @@ def predict_bboxes(img: List[Image.Image]) -> List[DataFrame]:
 
 
 def count_animals(old_bboxes: DataFrame, new_bboxes: DataFrame) -> int:
+    """
+    Count animals by difference between 2 dataframe with bboxes.
+    It returns number of new animals in new image
+    :param old_bboxes: pd.DataFrame
+    :param new_bboxes: pd.DataFrame
+    :return: int
+    """
     def cond(x, y) -> bool:
-        threshold = 1000
+        """
+        return condition whether 2 bboxes are similar
+        Similarity can be changed, but for now it is manhattan distance between bboxes less than threshold.
+        :param x: iloc indexer of pd.DataFrame, indicating bbox
+        :param y: iloc indexer of pd.DataFrame, indicating bbox
+        :return: bool
+        """
+        threshold = 1500
         x = torch.from_numpy(x.to_numpy())
         y = torch.from_numpy(y.to_numpy())
         s = torch.sum(torch.abs(x - y)).item()
-        logger.info(s)
         return s < threshold
 
+    # temporary data structures that saves whether certain bbox in similar to bbox from previous image
     bbox = new_bboxes.copy()
+    prev_bbox = old_bboxes.copy()
+    prev_bbox['prev'] = False
     bbox['prev'] = False
     for i in range(len(old_bboxes)):
         for j in range(len(new_bboxes)):
-            if bbox.iloc[j]['prev']:
+            # if bbox is already similar to bbox from previous image, then we don't need to check this bbox
+            if bbox.iloc[j]['prev'] or prev_bbox.iloc[i]['prev']:
                 continue
+                # similarity between bboxes from previous and present images
             if cond(old_bboxes.iloc[i], new_bboxes.iloc[j]):
+                # save status of similarity
+                prev_bbox['prev'][i] = True
                 bbox['prev'][j] = True
-    return bbox[bbox['prev'] == True].size.item()
+                # return number of non-similar(new animals) bboxes
+    return bbox[bbox['prev'] == False].size.item()
 
 
 def predict_sequence(bboxes: List[DataFrame]):
+    # first image's bboxes are all animals
     count = bboxes[0].shape[0]
     logger.info(f'image 0: {count} new animals')
     for i in range(len(bboxes) - 1):
+        # check the difference between two images and return new animals
         count += count_animals(bboxes[i], bboxes[i + 1])
         logger.info(f'image {i + 1}: {count} new animals')
     return count
@@ -55,6 +87,11 @@ def predict_sequence(bboxes: List[DataFrame]):
 
 @app.post('/predict/image')
 def predict(files: List[UploadFile] = File(...)):
+    """
+    Prediction of number of animals in sequence of images.
+    :param files: list[str], str is path to file
+    :return: int
+    """
     if not all([file.filename.split('.')[-1] in ['jpg', 'jpeg', 'png'] for file in files]):
         return 'Image must be jpg or png format!'
     images = [Image.open(file.file) for file in files]
